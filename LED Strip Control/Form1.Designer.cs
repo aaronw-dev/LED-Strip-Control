1fc622213e18411726c102ce7798f8b47eed3442
AW_Dev <riptidegamesceo@gmail.com>
2022-09-13 07:02:42 -0700

    All RGB Control works, no red lighting?

---
+++ LED Strip Control/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿using System.Windows.Forms;
+namespace LEDPeripheralControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.apply = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.brightnessBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Test";
+            this.notifyIcon1.Text = "Test";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.apply);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.brightnessBar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 166);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lighting";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Select Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ChangeColor);
+            // 
+            // apply
+            // 
+            this.apply.Location = new System.Drawing.Point(69, 137);
+            this.apply.Name = "apply";
+            this.apply.Size = new System.Drawing.Size(75, 23);
+            this.apply.TabIndex = 4;
+            this.apply.Text = "apply";
+            this.apply.UseVisualStyleBackColor = true;
+            this.apply.Click += new System.EventHandler(this.apply_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "100%";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0%";
+            // 
+            // brightnessBar
+            // 
+            this.brightnessBar.LargeChange = 1;
+            this.brightnessBar.Location = new System.Drawing.Point(3, 41);
+            this.brightnessBar.Maximum = 255;
+            this.brightnessBar.Name = "brightnessBar";
+            this.brightnessBar.Size = new System.Drawing.Size(268, 45);
+            this.brightnessBar.SmallChange = 0;
+            this.brightnessBar.TabIndex = 1;
+            this.brightnessBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.brightnessBar.Value = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Brightness";
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.BackColor = System.Drawing.Color.Firebrick;
+            this.closeButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.closeButton.Location = new System.Drawing.Point(289, -3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(19, 21);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "x";
+            this.closeButton.Click += new System.EventHandler(this.label1_Click);
+            this.closeButton.MouseEnter += new System.EventHandler(this.BtnHover);
+            this.closeButton.MouseLeave += new System.EventHandler(this.BtnLeave);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 234);
+            this.ControlBox = false;
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "LED Strip Control";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NotifyIcon notifyIcon1;
+        private GroupBox groupBox1;
+        private Label closeButton;
+        private Label label1;
+        private TrackBar brightnessBar;
+        private Label label3;
+        private Label label2;
+        private Button apply;
+        private Button button1;
+    }
+}
---
+++ LED Strip Control/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LEDPeripheralControl
+{
+    public partial class MainForm : Form
+    {
+        #region Window Drag
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+        SerialPort port = new SerialPort("COM6", 9600);
+
+        Color curColor;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            port.Open();
+            curColor = Color.White;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create the NotifyIcon.
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Activate();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public void BtnHover(object sender, EventArgs e)
+        {
+            closeButton.BackColor = Color.Red;
+        }
+        public void BtnLeave(object sender, EventArgs e)
+        {
+            closeButton.BackColor = Color.Firebrick;
+        }
+
+        private void apply_Click(object sender, EventArgs e)
+        {
+            float r = curColor.R;
+            float g = curColor.G;
+            float b = curColor.B;
+
+
+            float brightness = (float)brightnessBar.Value / 255;
+            r *= brightness;
+            g *= brightness;
+            b *= brightness;
+
+            Console.WriteLine("RD" + r + ",GN" + g + ",BE" + b + ",");
+            port.WriteLine("RD" + r + ",GN" + g + ",BE" + b + ",");
+        }
+
+        private void ChangeColor(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                curColor = dialog.Color;
+            }
+        }
+    }
+}
